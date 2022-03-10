180731bb38c1b5a6b9c3bd7dd012813b2f58acbd
Shotz5 <kitsulalex@gmail.com>
2022-03-10 14:28:49 -0800

    One dataset loaded, commit place in time

--- pull_data.py
+++ pull_data.py
@@ -1,20 +1,37 @@
 import pandas
-import nasdaqdatalink
 import json
 import numpy
+import requests
 
 def main():
     data = pandas.read_csv("BP_metadata.csv")
 
-    # for line in data.code:
-    #     print(line)
+    with open("data.json", "r") as json_file:
+        parent = json.load(json_file)
 
-    nasdaqdatalink.api_key = 'AqAMK4Qe87CQRbnA_qbV'
-    nasdaqdatalink.get("BP/" + data.code[1])
+    # First 1800
+    for current_ds in data.code[:1800]:
+        print(current_ds)
+        url = "https://data.nasdaq.com/api/v3/datasets/BP/" + current_ds + "?api_key=AqAMK4Qe87CQRbnA_qbV"
+        response = requests.get(url)
+        parent[current_ds] = response.json()
 
-    # with open('data.json', "w", encoding='utf-8') as json_file:
-    #     json_file.truncate(0)
-    #     json.dump(nneato, json_file, separators=(',', ':'), ensure_ascii=False, sort_keys=True, indent=4)
+    # After 1800
+    # for current_ds in data.code[1800:]:
+    #     print(current_ds)
+    #     # url = "https://data.nasdaq.com/api/v3/datasets/BP/" + current_ds + "?api_key=AqAMK4Qe87CQRbnA_qbV"
+    #     # response = requests.get(url)
+    #     # parent[current_ds] = response.json()
+
+    print("Outputting to file")
+    with open('data.json', "w") as json_file:
+        json.dump(parent, json_file, ensure_ascii=False, indent=4)
+
+def json_test():
+    url = "http://api.open-notify.org/astros.json"
+    response = requests.get(url)
+    res_json = response.json()
+    print(json.dumps(res_json, indent=2))
 
 if __name__ == "__main__":
     main()
